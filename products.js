97e999c470cdb1c5753958c2953e289d97ab1b1b
scottish5 <106618470+scottish5@users.noreply.github.com>
2026-04-23 17:21:32 -0400

    Update products.js

--- products.js
+++ products.js
@@ -3,84 +3,84 @@ const products = [
         id: 1,
         name: "Glow Serum",
         price: 29.99,
-        image: "https://images.unsplash.com/photo-1556228578-8c89e6adf883?w=500&h=500&fit=crop",
+        image: "../images/glow-serum.jpg",
         description: "Luminous facial serum for radiant skin"
     },
     {
         id: 2,
         name: "Hydra Moisturizer",
         price: 24.99,
-        image: "https://images.unsplash.com/photo-1556228720-03f89fab778c?w=500&h=500&fit=crop",
+        image: "../images/hydra-moisturizer.jpg",
         description: "Deep hydrating daily moisturizer"
     },
     {
         id: 3,
         name: "Lip Plumper",
         price: 19.99,
-        image: "https://images.unsplash.com/photo-1599643478518-a784e5dc4c8f?w=500&h=500&fit=crop",
+        image: "../images/lip-plumper.jpg",
         description: "Volumizing lip gloss with shine"
     },
     {
         id: 4,
         name: "Mascara Pro",
         price: 22.50,
-        image: "https://images.unsplash.com/photo-1563241571-3bbeabf74c0f?w=500&h=500&fit=crop",
+        image: "../images/mascara-pro.jpg",
         description: "Waterproof volumizing mascara"
     },
     {
         id: 5,
         name: "Foundation Base",
         price: 34.99,
-        image: "https://images.unsplash.com/photo-1570545887323-be4f7c5f7840?w=500&h=500&fit=crop",
+        image: "../images/foundation-base.jpg",
         description: "Full coverage liquid foundation"
     },
     {
         id: 6,
         name: "Blush & Glow",
         price: 18.99,
-        image: "https://images.unsplash.com/photo-1586271067483-beaafc0f4eaa?w=500&h=500&fit=crop",
+        image: "../images/blush-glow.jpg",
         description: "Rosy cream blush for cheeks"
     },
     {
         id: 7,
         name: "Eye Shadow Palette",
         price: 26.99,
-        image: "https://images.unsplash.com/photo-1609569269519-9b34c95e7f8f?w=500&h=500&fit=crop",
+        image: "../images/eyeshadow-palette.jpg",
         description: "12-color professional palette"
     },
     {
         id: 8,
         name: "Perfume Essence",
         price: 45.99,
-        image: "https://images.unsplash.com/photo-1595522052444-e76b51a3d358?w=500&h=500&fit=crop",
+        image: "../images/perfume-essence.jpg",
         description: "Long-lasting floral fragrance"
     },
     {
         id: 9,
         name: "Hair Serum",
         price: 21.99,
-        image: "https://images.unsplash.com/photo-1625948515291-69613efd103f?w=500&h=500&fit=crop",
+        image: "../images/hair-serum.jpg",
         description: "Silky hair smoothing serum"
     },
     {
         id: 10,
         name: "Sunscreen SPF 50",
         price: 23.99,
-        image: "https://images.unsplash.com/photo-1581577284212-2f2b39e0c76a?w=500&h=500&fit=crop",
+        image: "../images/sunscreen.jpg",
         description: "Daily UV protection lotion"
     },
     {
         id: 11,
         name: "Lip Balm Set",
         price: 14.99,
-        image: "https://images.unsplash.com/photo-1610009335033-ba0e4cbb9794?w=500&h=500&fit=crop",
+        image: "../images/lip-balm-set.jpg",
         description: "Pack of 3 flavored lip balms"
     },
     {
         id: 12,
         name: "Face Cleanser",
         price: 19.99,
-        image: "https://images.unsplash.com/photo-1556229287-1d7a3a36bef5?w=500&h=500&fit=crop",
+        image: "../images/face-cleanser.jpg",
         description: "Gentle daily facial cleanser"
     }
 ];
